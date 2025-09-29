b9c8e23cde53916c84e383ada8d087b02f2b137b
Crescencio Corpas <ccorpasmena@gmail.com>
2025-09-29 17:37:28 +0200

    cambiando la excepcion

--- instituto/src/main/java/es/iesjandula/instituto/utils/MiExcepcion.java
+++ instituto/src/main/java/es/iesjandula/instituto/utils/MiExcepcion.java
@@ -7,8 +7,8 @@ public class MiExcepcion extends Exception
 	 * 
 	 */
 	private static final long serialVersionUID = 4658336415478837157L;
-	
-	public MiExcepcion (String string) 
+
+	public MiExcepcion(String string)
 	{
 		super(string);
 	}
